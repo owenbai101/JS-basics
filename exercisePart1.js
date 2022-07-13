8c961f00f9cd9e29bd61fec1ca03412edb60b78d
owenbai <owenbai101@gmail.com>
2022-07-13 22:10:21 +0800

    13/07/2022

--- exercisePart1.js
+++ exercisePart1.js
@@ -49,3 +49,38 @@ function checkSpeed(speed)
             console.log('Points : ', points);
     }
 }
+
+
+function counttruthy(array){
+let count = 0;
+for(let value of array)
+{
+    if(value)
+        count++;
+    return count;
+}
+}
+
+
+function showproperties(obj)
+{
+    for(let key in obj)
+    {
+        if(typeof obj[key]==='string')
+            console.log(key,obj[key])
+    }
+}
+
+
+function sum(limit)
+{
+    let sum = 0
+    
+    for(let i = 0;i <= limit; i++)
+    {
+        if(i % 3 ===0 || i % 5 === 0)
+            sum += i;
+    }
+
+    return sum;
+}
---
+++ test.js
@@ -0,0 +1,13 @@
+let movie = {
+    title: 'owen',
+    releaseYear: 2019,
+    rating: 4.6,
+    diretor: 'abc'
+}
+
+for(let key in movie)
+{
+    console.log(key);
+    console.log(movie[key])
+}
+
