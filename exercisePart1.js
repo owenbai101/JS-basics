995a99bf2e6ce736776422e59ca0bd1f8d65dd65
owen <owenbai101@gmail.com>
2022-11-05 22:42:10 +0800

    20221105

--- exercisePart1.js
+++ exercisePart1.js
@@ -84,3 +84,72 @@ function sum(limit)
 
     return sum;
 }
+
+
+
+const marks =[80,80,50]
+
+console.log(calculateGrade(marks))
+
+function calculateGrade(marks){
+    const average = calculateAverage(marks);
+
+    if(average < 60)
+        return 'F';
+    if(average< 90)
+        return 'B';
+    return 'A';
+}
+function calculateAverage(array)
+{
+    let sum = 0;
+    for (let value of array)
+    {
+        sum += value;
+    }
+    return sum / array.count;
+}
+
+function showStars(rows){
+    for(let row = 1; row <= rows; row++){
+        let pattern = '';
+        for (let i = 0; i < row ; i++)
+        {
+            pattern += '*';
+            
+        } 
+        console.log(pattern);
+    }
+}
+
+showStars(7);
+
+
+function showPrimes(limit)
+{
+    for(let number =2; number <= limit; number++)
+    {
+        let isPrime = true;
+        for(let factor = 2; factor < number; factor++)
+            if(number%factor ===0)
+            break;
+    }
+    if(isPrime) console.log(number);
+}
+
+
+function showPrimes2(limit)
+{
+    for(let number =2; number <= limit; number++)
+        if(isPrime(number))
+            console.log(number);
+}
+
+function isPrime(number)
+{
+    if(number%factor ===0)
+    for(let factor = 2; factor < number; factor++)
+        return false
+        
+    return true;
+}
