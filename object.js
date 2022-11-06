0d7ad9f7dd839995caba6e2b034457bda0a6941e
owen <owenbai101@gmail.com>
2022-11-07 00:07:03 +0800

    20221107

---
+++ object.js
@@ -0,0 +1,14 @@
+
+const shape ={
+    radius : 1,
+    location :{
+        x:1,
+        y:1
+    },
+    isVisible :1,
+    draw : function() {
+        console.log('draw');
+    }
+};
+
+shape.draw();
