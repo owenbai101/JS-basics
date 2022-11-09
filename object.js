e1a1d967752f02a09972e3167938f88f4e5d3080
owenbai <owenbai101@gmail.com>
2022-11-09 22:41:30 +0800

    20221109

--- object.js
+++ object.js
@@ -40,7 +40,7 @@ function Circle(radius)
 }
 
 const circle = new Circle(1);
-
+circle.draw();
 
 let customer={
     name: 'owen'
@@ -52,3 +52,53 @@ customer.address ='abcd';
 delete customer.tel;
 
 console.log(customer);
+
+//------------------------------------------------------------------------------
+//Enumerating Properties of an Object
+const bigCircle ={
+    radius : 5,
+    drawNow(){
+        console.log('draw');
+    }
+} ;
+
+for(let key in bigCircle)
+    console.log(key, bigCircle[key]);
+
+for(let key of Object.keys(bigCircle))
+    console.log(key);
+
+for(let entry of Object.entries(bigCircle))
+    console.log(entry);
+
+if('radius' in circle) console.log('yes');
+
+//-----------------------------------------------------------------------------------------
+//cloning a object 
+
+const smallCircle ={
+    radius : 5,
+    drawNow(){
+        console.log('draw');
+    }
+} ;
+
+const another ={};
+
+for (let key in smallCircle)
+    another[key] = smallCircle[key];
+
+    console.log(another);
+//second method
+
+const another2 = Object.assign({},circle);
+
+console.log(another2)
+
+//third method
+
+const another3 ={...circle};
+
+console.log(another3)
+//-----------------------------------------------------------------------------------------
+//garbage collection 
