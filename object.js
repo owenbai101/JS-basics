669db58fb5eb744fbb820f82effb83db707ba946
owen 2 <owenbai101@gmail.com>
2022-11-09 11:50:30 +0800

    new

--- object.js
+++ object.js
@@ -12,3 +12,43 @@ const shape ={
 };
 
 shape.draw();
+
+
+//Factor funtion
+
+function createCircle(radius){
+    return{
+        radius,   // same as radius: radius
+        draw() {
+            console.log('draw');
+        }
+    };
+}
+
+const circle2 = createCircle(2);
+console.log(circle2);
+
+
+// Constructor Function
+
+function Circle(radius)
+{
+    this.radius =radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1);
+
+
+let customer={
+    name: 'owen'
+};
+
+customer.tel = '12345';
+customer.address ='abcd';
+
+delete customer.tel;
+
+console.log(customer);
